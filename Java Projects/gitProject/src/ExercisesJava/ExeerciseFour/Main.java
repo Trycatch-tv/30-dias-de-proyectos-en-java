cc473af2970c3227ab5ec5975e2ab93312aa6d13
Jjuanki <juancaranqui2dobgu@gmail.com>
2024-08-07 19:27:13 -0500

    Hasta el ejercicio 4

---
+++ Java Projects/gitProject/src/ExercisesJava/ExeerciseFour/Main.java
@@ -0,0 +1,14 @@
+package ExercisesJava.ExeerciseFour;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Util u = new Util();
+
+        u.mainMenu();
+        
+
+    }
+    
+}
---
+++ Java Projects/gitProject/src/ExercisesJava/ExeerciseFour/Unidades.java
@@ -0,0 +1,24 @@
+package ExercisesJava.ExeerciseFour;
+
+public class Unidades {
+
+     private double km = 1;
+     private double cm = 1;
+     private double m = 1;
+     private double yd = 1;
+    public double getKm() {
+        return km;
+    }
+    public double getCm() {
+        return cm;
+    }
+    public double getM() {
+        return m;
+    }
+    public double getYd() {
+        return yd;
+    }
+
+
+     
+}
