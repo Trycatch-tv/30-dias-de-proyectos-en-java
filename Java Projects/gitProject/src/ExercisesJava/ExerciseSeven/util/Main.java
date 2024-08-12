3fee37c4d8e4c10c15cbec147493d69b355d29b2
Jjuanki <juancaranqui2dobgu@gmail.com>
2024-08-11 20:40:04 -0500

    AHDJASJD

--- Java Projects/gitProject/src/ExercisesJava/ExerciseSeven/util/Main.java
+++ Java Projects/gitProject/src/ExercisesJava/ExerciseSeven/util/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hola!!!");
 		
-		System.out.println("Última pruebaaaaaaaaaaaaaaa"); 
+		System.out.println("Primer prueba"); 
 
 
 
